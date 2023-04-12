654bc9c526703da0dec40862f72c12797454a9f9
katsapov.sergey <katsapov.s@promail366.com>
2023-04-12 16:28:00 +0300

    [ADD] - add test spinner/list view logic;

--- app/src/main/java/com/example/tms_android_project/ui/fragments/NewsFragment.kt
+++ app/src/main/java/com/example/tms_android_project/ui/fragments/NewsFragment.kt
@@ -1,11 +1,17 @@
 package com.example.tms_android_project.ui.fragments
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.tms_android_project.databinding.FragmentNewsBinding
+
 
 class NewsFragment : Fragment() {
 
@@ -17,5 +23,73 @@ class NewsFragment : Fragment() {
     ): View {
         binding = FragmentNewsBinding.inflate(inflater, container, false);
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        //initSpinner()
+        //initListView()
+    }
+
+
+    private fun initSpinner() {
+        //TEST --> Simple example
+//        val selected = binding.spinner.selectedItem.toString()
+//        Toast.makeText(context, selected, Toast.LENGTH_SHORT).show()
+
+        //TEST --> Adapter
+//        val adapter: ArrayAdapter<*> = ArrayAdapter.createFromResource(requireContext(),
+//            com.example.tms_android_project.R.array.cars, R.layout.simple_spinner_item)
+//        adapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
+
+        //вызов адаптера
+        //binding.spinner.setAdapter(adapter)
+
+
+
+        //TEST ---> onItemSelectedListener
+//        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+//            override fun onItemSelected(
+//                parent: AdapterView<*>?,
+//                itemSelected: View, selectedItemPosition: Int, selectedId: Long
+//            ) {
+//                val choose = resources.getStringArray(com.example.tms_android_project.R.array.cars)
+//                val toast = Toast.makeText(
+//                    requireContext(),
+//                    choose[selectedItemPosition], Toast.LENGTH_SHORT
+//                )
+//                toast.show()
+//            }
+//
+//            override fun onNothingSelected(parent: AdapterView<*>?) {}
+//        }
+
+
+        //TEST --> выбор выбранного элемента
+        //binding.spinner.setSelection(2)
+    }
+
+    private fun initListView(){
+//        val adapter = ArrayAdapter(
+//            requireContext(),
+//            R.layout.simple_list_item_1,  resources.getStringArray(com.example.tms_android_project.R.array.cars_list)
+//        )
+//
+//        //прослушивание нажатий
+//        binding.listView.setOnItemClickListener(OnItemClickListener { parent, itemClicked, position, id ->
+//            Toast.makeText(
+//                requireContext(),
+//                (itemClicked as TextView).text,
+//                Toast.LENGTH_SHORT
+//            ).show()
+//        })
+//
+//        // Привяжем массив через адаптер к ListView
+//        binding.listView.setAdapter(adapter)
     }
 }
