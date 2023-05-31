9a4bbb5536e7688d180f815686b6e6f3c94e740c
sergey_katsapov <siarhei.katsapau@gmail.com>
2023-05-31 08:18:09 +0300

    [ADD] - remove test fragment logic;

--- app/src/main/java/com/example/tms_android_project/ui/presentation/view_models/TestViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.tms_android_project.ui.presentation.view_models
-
-import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-
-@HiltViewModel
-class TestViewModel @Inject constructor() : ViewModel()
