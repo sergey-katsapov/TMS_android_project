f027b1a56972274d64110733768cfad8ed20925d
sergey_katsapov <siarhei.katsapau@gmail.com>
2023-06-14 16:48:24 +0300

    [ADD] - removed duplicates;

--- app/src/main/java/com/example/tms_android_project/ui/di/module/UseCasesModule.kt
+++ app/src/main/java/com/example/tms_android_project/ui/di/module/UseCasesModule.kt
@@ -3,6 +3,7 @@ package com.example.tms_android_project.ui.di.module
 import com.example.tms_android_project.ui.domain.repositories.PostRepository
 import com.example.tms_android_project.ui.domain.use_cases.GetPostInfoUseCase
 import com.example.tms_android_project.ui.domain.use_cases.PostsUseCase
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -12,8 +13,4 @@ class UseCasesModule {
     @Provides
     fun providePostInfoUseCase(postRepository: PostRepository) =
         GetPostInfoUseCase(postRepository = postRepository)
-
-    @Provides
-    fun providePostsUseCase(postRepository: PostRepository) =
-        PostsUseCase(postRepository = postRepository)
 }
